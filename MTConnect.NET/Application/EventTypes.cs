75d7d161008967536b73a8298cffb3b938bd6f76
Patrick Ritchie <Patrick@feenux.com>
2016-05-25 18:39:58 -0400

    Exposed EventTypes.Get() to return a list of Event Type Values

---
+++ MTConnect.NET/Application/EventTypes.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System.Data;
+
+namespace MTConnect.Application
+{
+    public class EventTypes
+    {
+        public static DataTable Get()
+        {
+            return Tools.Tables.GetEventTypes();
+        }
+
+    }
+}
