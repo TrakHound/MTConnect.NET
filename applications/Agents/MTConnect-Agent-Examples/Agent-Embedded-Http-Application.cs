3ef981d07dafa00b48896c3e8eb8b40e890b0847
Patrick Ritchie <patrick@trakhound.com>
2022-07-13 00:23:35 -0400

    Updated Agent Examples

---
+++ applications/Agents/MTConnect-Agent-Examples/Agent-Embedded-Http-Application.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2022 TrakHound Inc., All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using MTConnect.Applications.Agents;
+using MTConnect.Observations.Events.Values;
+using MTConnect.Observations.Input;
+
+namespace MTConnect.Applications
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var agentApplication = new MTConnectHttpAgentApplication();
+            agentApplication.Run(args);
+
+
+            // Start Adding Observations directly to Agent
+            agentApplication.Agent.AddObservation("VMC-3Axis", new ObservationInput("avail", Availability.AVAILABLE));
+            agentApplication.Agent.AddObservation("VMC-3Axis", new ObservationInput("estop", EmergencyStop.ARMED));
+            agentApplication.Agent.AddObservation("VMC-3Axis", new ObservationInput("mode", ControllerMode.AUTOMATIC));
+            agentApplication.Agent.AddObservation("VMC-3Axis", new ObservationInput("execution", Execution.ACTIVE));
+
+
+            var i = 0;
+            while (true)
+            {
+                agentApplication.Agent.AddObservation("VMC-3Axis", new ObservationInput("Xact", i++));
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
