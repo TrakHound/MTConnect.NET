4b7a5b41927d995ab42c8f3ee88c91bc4826e428
Patrick Ritchie <patrick@trakhound.com>
2023-01-20 14:08:50 -0500

    Updated file header date from 2022 to 2023

--- src/MTConnect.NET-Common/Devices/Configurations/Specifications/ControlLimits.cs
+++ src/MTConnect.NET-Common/Devices/Configurations/Specifications/ControlLimits.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 TrakHound Inc., All Rights Reserved.
+// Copyright (c) 2023 TrakHound Inc., All Rights Reserved.
 
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
