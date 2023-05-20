4c20509ae2e2ae6d7395a860c3b3a64ad08832b9
Patrick Ritchie <patrick@trakhound.com>
2023-05-20 12:57:45 -0400

    Updated Assembly Version to 5.4.3

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.4.2")]
-[assembly: AssemblyFileVersion("5.4.2")]
+[assembly: AssemblyVersion("5.4.3")]
+[assembly: AssemblyFileVersion("5.4.3")]
 [assembly: AssemblyCompany("TrakHound Inc.")]
 [assembly: AssemblyCopyright("Copyright (c) 2023 TrakHound Inc., All Rights Reserved.")]
