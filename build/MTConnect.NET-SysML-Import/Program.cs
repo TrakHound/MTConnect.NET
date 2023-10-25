9ce374a95d8396f47cce5c89b59ce8e7caf31a9d
Patrick Ritchie <patrick@trakhound.com>
2023-10-24 20:55:49 -0400

    Added SysML Import Project

---
+++ build/MTConnect.NET-SysML-Import/Program.cs
@@ -0,0 +1,18 @@
+﻿using MTConnect.SysML.CSharp;
+using MTConnect.SysML;
+using System.Text.Json;
+
+var xmlPath = @"D:\TrakHound\MTConnect\MTConnectSysMLModel.xml";
+var outputPath = @"C:\temp\mtconnect-model-results";
+
+var mtconnectModel = MTConnectModel.Parse(xmlPath);
+
+var jsonOptions = new JsonSerializerOptions();
+jsonOptions.WriteIndented = true;
+jsonOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault;
+jsonOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+var json = JsonSerializer.Serialize(mtconnectModel, options: jsonOptions);
+await File.WriteAllTextAsync(@"C:\temp\mtconnect-model.json", json);
+
+CSharpTemplateRenderer.Render(mtconnectModel, outputPath);
