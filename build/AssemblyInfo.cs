576e1a6b0da4ac8a98be48116e55433c611a7095
Patrick Ritchie <patrick@trakhound.com>
2023-12-12 02:16:32 -0500

    Update assembly version to 6.0.3

--- build/AssemblyInfo.cs
+++ build/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("6.0.1")]
-[assembly: AssemblyFileVersion("6.0.1")]
+[assembly: AssemblyVersion("6.0.3")]
+[assembly: AssemblyFileVersion("6.0.3")]
 [assembly: AssemblyCompany("TrakHound Inc.")]
 [assembly: AssemblyCopyright("Copyright (c) 2023 TrakHound Inc., All Rights Reserved.")]
