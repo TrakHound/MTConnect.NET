ff7a914b0a29722c1871e4f047fc90e78630e524
Patrick Ritchie <patrick@trakhound.com>
2023-12-12 02:00:02 -0500

    Updated MqttServerConfiguration

--- libraries/MTConnect.NET-MQTT/Configurations/IMTConnectMqttServerConfiguration.cs
+++ libraries/MTConnect.NET-MQTT/Configurations/IMTConnectMqttServerConfiguration.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) 2023 TrakHound Inc., All Rights Reserved.
+// TrakHound Inc. licenses this file to you under the MIT license.
 
 namespace MTConnect.Configurations
 {
     public interface IMTConnectMqttServerConfiguration
     {
+        /// <summary>
+        /// Sets the Interval (in milliseconds) to send Current messages at
+        /// </summary>
         int CurrentInterval { get; }
 
+        /// <summary>
+        /// Sets the Interval (in milliseconds) to send Sample messages at
+        /// </summary>
         int SampleInterval { get; }
-
-
-        string ProbeTopic { get; }
-
-        string CurrentTopic { get; }
-
-        string SampleTopic { get; }
-
-        string AssetTopic { get; }
     }
 }
