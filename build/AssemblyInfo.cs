e72d8a59345fd6bd853bc3c1998fa3344075878c
Patrick Ritchie <patrick@trakhound.com>
2024-08-06 23:57:11 -0400

    Update v6.4.6
    - Fixed issue with SHDR Ping/Pong Heartbeat referenced in Issue #70
    - Added IgnoreHeartbeatOnChange configuration parameter for SHDR Adapters to toggle whether PING data is sent even if data has been received.

--- build/AssemblyInfo.cs
+++ build/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("6.4.5")]
-[assembly: AssemblyFileVersion("6.4.5")]
+[assembly: AssemblyVersion("6.4.6")]
+[assembly: AssemblyFileVersion("6.4.6")]
 [assembly: AssemblyCompany("TrakHound Inc.")]
 [assembly: AssemblyCopyright("Copyright (c) 2024 TrakHound Inc., All Rights Reserved.")]
