67d2be0556801fb6be5288776a25a078414e3a02
Patrick Ritchie <Patrick@TrakHound.com>
2025-09-13 11:39:49 -0400

    Updated MQTTnet to 4.3.7 and removed the unused reference to MQTTnet.Extensions.ManagedClient

--- build/AssemblyInfo.cs
+++ build/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("6.7.0")]
-[assembly: AssemblyFileVersion("6.7.0")]
+[assembly: AssemblyVersion("6.8.0")]
+[assembly: AssemblyFileVersion("6.8.0")]
 [assembly: AssemblyCompany("TrakHound Inc.")]
 [assembly: AssemblyCopyright("Copyright (c) 2025 TrakHound Inc., All Rights Reserved.")]
