088e95cf1f9f860fd625aced6ed8e6b9d1acf682
Patrick Ritchie <Patrick@TrakHound.com>
2025-10-16 13:32:48 -0400

    Delete AssemblyInfo.cs

--- build/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyVersion("6.8.0")]
-[assembly: AssemblyFileVersion("6.8.0")]
-[assembly: AssemblyCompany("TrakHound Inc.")]
-[assembly: AssemblyCopyright("Copyright (c) 2025 TrakHound Inc., All Rights Reserved.")]
