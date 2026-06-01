e6217392d8e26d1f91521890b440e5d370b5b979
Otto Bolyós <obolyos@mriiot.com>
2026-06-01 12:17:16 +0200

    docs(templates): author XML doc comments on embedded-agent template

--- templates/mtconnect.net-agent/content/MTConnect.NET-Embedded-Agent/Program.cs
+++ templates/mtconnect.net-agent/content/MTConnect.NET-Embedded-Agent/Program.cs
@@ -7,6 +7,7 @@ using MTConnect.Observations.Events;
 
 namespace MTConnectAgent
 {
+    /// <summary>Represents the program.</summary>
     public class Program
     {
         static void Main(string[] args)
@@ -16,19 +17,27 @@ namespace MTConnectAgent
         }
 
 
+        /// <summary>Represents the module configuration.</summary>
         public class ModuleConfiguration : DataSourceConfiguration
         {
+            /// <summary>Gets or sets the plc address.</summary>
             public string PlcAddress { get; set; }
         }
 
 
+        /// <summary>Represents the data source module.</summary>
         public class DataSourceModule : MTConnectInputAgentModule
         {
+            /// <summary>The configuration type id.</summary>
             public const string ConfigurationTypeId = "datasource"; // This must match the module section in the 'agent.config.yaml' file
+            /// <summary>The default id.</summary>
             public const string DefaultId = "DataSource Module"; // The ID is mainly just used for logging.
             private readonly ModuleConfiguration _configuration;
 
 
+            /// <summary>Initialises a new instance of the data source module type.</summary>
+            /// <param name="agent">The agent.</param>
+            /// <param name="configuration">The configuration.</param>
             public DataSourceModule(IMTConnectAgentBroker agent, object configuration) : base(agent)
             {
                 Id = DefaultId;
@@ -38,6 +47,8 @@ namespace MTConnectAgent
             }
 
 
+            /// <summary>Runs the on add device operation.</summary>
+            /// <returns>The result of the operation.</returns>
             protected override IDevice OnAddDevice()
             {
                 var device = new Device();
@@ -52,6 +63,7 @@ namespace MTConnectAgent
             }
 
 
+            /// <summary>Runs the on read operation.</summary>
             protected override void OnRead()
             {
                 Log(MTConnect.Logging.MTConnectLogLevel.Information, $"Read PLC Data from ({_configuration.PlcAddress})");
